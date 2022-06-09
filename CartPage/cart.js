57838944e1c990f066b59c4888f10adac6fd31bb
yousef mahmoud <yousefmahmoud150@gmail.com>
2022-06-09 18:03:24 +0200

    add delete product function and make cart page animation after delete the product

--- CartPage/cart.js
+++ CartPage/cart.js
@@ -8,30 +8,59 @@ window.onload = ()=> {
 }
 
 const getProductsFromLoclStrg = ()=> {
+    cartProducts.innerHTML = ''
     const myCart = JSON.parse(localStorage.getItem('cart'));
-    
-    myCart.forEach(product=> {
-        const theProduct = document.createElement('div')
-        theProduct.innerHTML = `
-            <div class="card" style="width: 14rem;">
-                <img src=${product.image} class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h6 class="card-title">${product.title}</h6>
-                        <p class="card-text">Pirce: ${product.price}$</p>
-                        <p class="card-text">Quantity: ${product.quantity}$</p>
-                        <p class="card-text">SubTotal: ${parseInt(product.quantity) * product.price}$</p>
-                        <button class="btn remove-product btn-danger">Remove</button>
-                    </div>
-            </div>
-            `
-            cartProducts.appendChild(theProduct)
-    })
+    if(myCart.length > 0){
 
-    const totalElement = document.querySelector('.total')
-    let cartTotal = myCart.map(item=> item.price * item.quantity).reduce((a,b)=> a + b)
-    totalElement.innerHTML = `Total : ${cartTotal}$`
+        myCart.forEach(product=> {
+            const theProduct = document.createElement('div')
+            theProduct.innerHTML = `
+                <div class="card" style="width: 14rem;">
+                    <img src=${product.image} class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h6 class="card-title">${product.title}</h6>
+                            <p class="card-text">Pirce: ${product.price}$</p>
+                            <p class="card-text">Quantity: ${product.quantity}$</p>
+                            <p class="card-text">SubTotal: ${parseInt(product.quantity) * product.price}$</p>
+                            <button data-productid=${product.id} class="btn remove-product btn-danger">Remove</button>
+                        </div>
+                </div>
+                `
+                cartProducts.appendChild(theProduct)
+        })
+        
+        
+        // Delete product Function
+        const delBtn = document.querySelectorAll('.remove-product')
+        delBtn.forEach(btn=> {
+            btn.addEventListener('click',(e)=> {
+                let productID = parseInt(e.target.getAttribute('data-productid'));
+                removeProduct(productID)
+            })
+        })
+
+        
+        const totalElement = document.querySelector('.total')
+        let cartTotal = myCart.map(item=> item.price * item.quantity).reduce((a,b)=> a + b)
+        totalElement.innerHTML = `Total : ${cartTotal}$`
+        
+    }
+    if(myCart.length < 1 ){
+        const totalElement = document.querySelector('.total')
+        totalElement.innerHTML =`Total : ${0}$`
+    }
 }
 
+const removeProduct = (id) => {
+    const animediv = document.querySelector('.anime')
+    animediv.classList.add('animationScann')
+    const myCart = JSON.parse(localStorage.getItem('cart')).filter(product=>product.id !== id )
+    localStorage.setItem('cart',JSON.stringify(myCart))
+    getProductsFromLoclStrg()
+    setTimeout(() => {
+        animediv.classList.remove('animationScann')
+    }, 3000);
+}
 
 openMenu.addEventListener('click',()=> {
     headerUl.style.right = '1px'
